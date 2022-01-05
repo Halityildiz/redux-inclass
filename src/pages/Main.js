04bdaf58d87fdb9b41e2007fb24a27e8b15e2c2b
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-05 22:18:25 +0300

    v1

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,42 +1,55 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
-import React from 'react';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserList } from "../App";
 
-const fakeUserList = [
-    { name: 'Ed', username: 'mred', email: 'mred@mail.com' },
-    { name: 'Felix', username: 'mrfelix', email: 'mrfelix@mail.com' },
-    { name: 'John', username: 'mrjohn', email: 'mrjohn@mail.com' },
-    { name: 'Tom', username: 'mrtom', email: 'mrtom@mail.com' },
-    { name: 'Jos', username: 'mrjos', email: 'mrjos@mail.com' },
-]
+// https://jsonplaceholder.typicode.com/users
 
 const Main = () => {
-    return (
-        <TableContainer component={Paper} >
-            <Table sx={{ minWidth: 650 }}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Username</TableCell>
-                        <TableCell>Email</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        fakeUserList.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell>{row.name}</TableCell>
-                                <TableCell>{row.username}</TableCell>
-                                <TableCell>{row.email}</TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
-}
+  const dispatch = useDispatch();
+  // const loading = useSelector(state => state.loading);
+  // const userList = useSelector(state => state.userList);
 
-export default Main
+  const { loading, userList } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(getUserList);
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Username</TableCell>
+            <TableCell>Email</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {userList.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.username}</TableCell>
+              <TableCell>{row.email}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default Main;
